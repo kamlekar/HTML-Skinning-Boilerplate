270d4a98cb47f5af78a5c58a04ec677244810836
Mr_Green <kamlekar.venkatesh@gmail.com>
2015-09-03 17:21:32 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,16 @@ var nunjucksRender = require('gulp-nunjucks-render');
 
 var assets_path = "assets/";
 
+/* // Test this function 
+   // Intention: Stops from breaking the gulp watch on any error
+function swallowError (error) {
+    //If you want details of the error in the console
+    console.log(error.toString());
+    notify(error.toString());
+    util.beep();
+    this.emit('end');
+}
+*/
 gulp.task('default', function () {
     nunjucksRender.nunjucks.configure(['templates/'], { watch: false });
     return gulp.src('templates/!(_)*.html')
@@ -11,6 +21,7 @@ gulp.task('default', function () {
         	js_path: assets_path + "js/",
         	img_path: assets_path + "images/"
         }))
+        // .on('error', swallowError)
         .pipe(gulp.dest('site'));
 });
 gulp.task('watch', function () {
