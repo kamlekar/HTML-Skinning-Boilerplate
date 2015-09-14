54a68ca1212ed682c517503257591d4d60ed045e
Mr_Green <kamlekar.venkatesh@gmail.com>
2015-09-14 20:38:21 +0530

    Added gulp svg store code

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
 
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var path = require('path');
+
+
+// svg group folder names present in "bundle-svgs" folder
+var svgs = ['xyz-sprt', 'abc-sprt', 'pqrs-sprt'];
+// - CD to the project folder
+// - Run "gulp <folder-name>" in CLI to generate the sprite svg
+for(var i = 0; i < svgs.length; i++){
+    gulp.task(svgs[i], function () {
+        return gulp
+            // looks for each folder inside "bundle-svgs" folder
+            .src('bundle-svgs/' + this + '/*.svg')
+            .pipe(svgmin(function (file) {
+                var prefix = path.basename(file.relative, path.extname(file.relative));
+                return {
+                    plugins: [{
+                        cleanupIDs: {
+                            prefix: prefix + '-',
+                            minify: true
+                        }
+                    }]
+                }
+            }))
+            .pipe(svgstore())
+            // Store the generated svg sprite in "site/assets/images/" folder
+            .pipe(gulp.dest('site/assets/images/'));
+    }.bind(svgs[i]));    
+}
+
 var assets_path = "assets/";
 
 /* // Test this function 
