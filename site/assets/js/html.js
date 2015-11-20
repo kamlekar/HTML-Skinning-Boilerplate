38eef26d53ec41589b57ec5185b916331c170d74
Mr_Green <venkateshwar@divami.com>
2015-11-20 14:31:43 +0530

    small changes

--- site/assets/js/html.js
+++ site/assets/js/html.js
@@ -23,26 +23,12 @@ document.addEventListener('mousedown', function(e){
 	// First check whether the user clicked on the clickable element(s) or target element(s).
 	// If user hasn't clicked on the clickable element or target element then reset the performed actions on the clickable element(s) and target element(s) with outside clicking is as active.
 	// (For this, we need to add another common attribute to clicked element and target element)
-	if(!dontCallDocumentClick){
-		console.log("document mousedown");
-
-		var resettableElements = Array.prototype.slice.call(htmlClickElements).filter(function(el){
-			for(var j = 0;j < ignorableElements.length; j++){
-				if(el === ignorableElements[j]){
-					return false;
-				}
-			}
-			return true;
-		})
-		for(var i = 0; i < resettableElements.length; i++){
-			performActions(resettableElements[i], true);
-		}
-	}
+	resetStyles();
 });
 
 // Defining outside click actions
 document.addEventListener('mouseup', function(e){
-	console.log("document mouseup");
+	
 });
 
 function performActions(clickedElement, doOpposite){
@@ -68,22 +54,30 @@ function performActions(clickedElement, doOpposite){
 		htmlActions(targetElements, toggleType, targetElementClass);
 
 		// add elements to ignore when document is clicked
-		var ignorableElements = Array.prototype.slice.call(targetElements);
+		ignorableElements = Array.prototype.slice.call(targetElements);
 		ignorableElements.push.apply(ignorableElements, Array.prototype.slice.call([clickedElement]));
 	}
 }
+
+function resetStyles(){
+	var resettableElements = Array.prototype.slice.call(htmlClickElements).filter(function(el){
+		for(var j = 0;j < ignorableElements.length; j++){
+			if(el === ignorableElements[j]){
+				return false;
+			}
+		}
+		return true;
+	})
+	for(var i = 0; i < resettableElements.length; i++){
+		performActions(resettableElements[i], true);
+	}
+}
+
 function htmlClick(e){
 	dontCallDocumentClick = true;
 	console.log("element mousedown");
 	var clickedElement = e.currentTarget;
 	performActions(clickedElement);
-	// Reset the performed actions on the other clickable element(s) with outside clicking is as active.
-	var allHtmlClickAndTargetElements = document.querySelectorAll('.html-click');
-	[].forEach.call(allHtmlClickAndTargetElements, function(el){
-		if(el !== clickedElement){
-			performActions(el, true);
-		}
-	});
 }
 
 function htmlActions(targetElements, toggleType, targetElementClass){
