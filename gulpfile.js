20dffeaf57fc72ffd180093411e528eb4755fac1
venkateshwar <kamlekar.venkatesh@gmail.com>
2022-04-10 23:46:22 +0530

    Update node to v12.22.6 and update the packages accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const mmq = require('gulp-merge-media-queries');
 const htmlCompilers = {
