52e2e366baaec48c88878929f99729db5886faf4
Venkateshwar <venkateshwar.kamlekar@estimateone.com>
2022-04-11 09:06:39 +0530

    gulpfile.js lint format

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
 const mmq = require('gulp-merge-media-queries');
 const htmlCompilers = {
     '.pug': require('gulp-pug'),
@@ -9,17 +8,15 @@ const htmlCompilers = {
 const sourcemaps = require('gulp-sourcemaps');
 const prettify = require('gulp-prettify');
 const svgstore = require('gulp-svgstore');
-const gutil = require('gulp-util');
 const svgmin = require('gulp-svgmin');
 const connect = require('gulp-connect');
 const path = require('path');
 const fs = require('fs');
 const rename = require('gulp-rename');
-const uncss = require('postcss-uncss');
 const clean = require('postcss-clean');
 const plumber = require('gulp-plumber');
-const browsersync = require("browser-sync").create();
-const cleanCSS = require("gulp-clean-css");
+const browsersync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
 const data = require('gulp-data');
 
 let config = require('./config.json');
@@ -32,7 +29,7 @@ var SVGS_SOURCE_PATH = 'bundle-svgs/';
 var SVGS_ALL_PATH = 'bundle-svgs/**/*.svg';
 var SASS_PATH = 'sass/**/*.scss';
 var CSS_PATH = 'dist/assets/css/';
-var ASSETS_PATH = "assets/";
+var ASSETS_PATH = 'assets/';
 var PRE_MAIN_TEMPLATES = (ext) => `templates/**/!(_)*${ext}`;
 var PRE_ALL_TEMPLATES = (ext) => `templates/**/*${ext}`;
 var JS_PATH = 'dist/assets/js/';
@@ -59,69 +56,82 @@ function generateSvg() {
     if (svgGrouping) {
         var svgKeys = Object.keys(svgGrouping);
         return Promise.all(
-            svgKeys.map(key => {
+            svgKeys.map((key) => {
                 var pageSpecificSVGs = svgGrouping[key]; // Array
                 // mapping the page specific variables with relative path
-                var newPageSpecificSVGs = pageSpecificSVGs.map(p => SVGS_SOURCE_PATH + p + '.svg');
-                return gulp
-                    .src(newPageSpecificSVGs)
-                    .pipe(plumber())
-                    .pipe(svgmin(function (file) {
-                        var prefix = path.basename(file.relative, path.extname(file.relative));
-                        return {
-                            plugins: [{
-                                cleanupIDs: {
-                                    prefix: prefix + '-',
-                                    minify: true
-                                }
-                            }]
-                        }
-                    }))
-                    .pipe(svgstore())
-                    // Uncomment the below lines to add additional attributes to the generated SVG Sprite
-                    // .pipe(cheerio(function($, file){
-                    //     $('svg > symbol').attr('preserveAspectRatio', 'xMinYMid');
-                    // }))
-                    .pipe(rename(key + '.svg'))
-                    // Store the generated svg sprite in "dist/assets/images/" folder
-                    .pipe(gulp.dest(IMAGES_PATH))
-                    .pipe(browsersync.stream());
+                var newPageSpecificSVGs = pageSpecificSVGs.map(
+                    (p) => SVGS_SOURCE_PATH + p + '.svg'
+                );
+                return (
+                    gulp
+                        .src(newPageSpecificSVGs)
+                        .pipe(plumber())
+                        .pipe(
+                            svgmin(function(file) {
+                                var prefix = path.basename(
+                                    file.relative,
+                                    path.extname(file.relative)
+                                );
+                                return {
+                                    plugins: [
+                                        {
+                                            cleanupIDs: {
+                                                prefix: prefix + '-',
+                                                minify: true
+                                            }
+                                        }
+                                    ]
+                                };
+                            })
+                        )
+                        .pipe(svgstore())
+                        // Uncomment the below lines to add additional attributes to the generated SVG Sprite
+                        // .pipe(cheerio(function($, file){
+                        //     $('svg > symbol').attr('preserveAspectRatio', 'xMinYMid');
+                        // }))
+                        .pipe(rename(key + '.svg'))
+                        // Store the generated svg sprite in "dist/assets/images/" folder
+                        .pipe(gulp.dest(IMAGES_PATH))
+                        .pipe(browsersync.stream())
+                );
             })
-        )
+        );
     }
     return new Promise((resolve, reject) => resolve({}));
-};
-
-
-
+}
 
 /*****************************************/
 /*******SASS precompiling function********/
 /*****************************************/
 // TO DO: Keep the compiled css code in expanded mode
 function sassChange() {
-    return gulp.src(SASS_PATH)
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        // TO DO: remove comments while compiling sass to css "sourceComments: false" doesn't work.
-        .pipe(sass({ outputStyle: 'expanded', sourceComments: false }))
-        .pipe(mmq({ log: true }))
-        // For mapping: Don't mention the path to make the mapping inline
-        .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest(CSS_PATH))
-        .pipe(rename({
-            suffix: ".min"
-        }))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(CSS_PATH))
-        .pipe(browsersync.stream());
+    return (
+        gulp
+            .src(SASS_PATH)
+            .pipe(plumber())
+            .pipe(sourcemaps.init())
+            // TO DO: remove comments while compiling sass to css "sourceComments: false" doesn't work.
+            .pipe(sass({ outputStyle: 'expanded', sourceComments: false }))
+            .pipe(mmq({ log: true }))
+            // For mapping: Don't mention the path to make the mapping inline
+            .pipe(sourcemaps.write('maps'))
+            .pipe(gulp.dest(CSS_PATH))
+            .pipe(
+                rename({
+                    suffix: '.min'
+                })
+            )
+            .pipe(cleanCSS())
+            .pipe(gulp.dest(CSS_PATH))
+            .pipe(browsersync.stream())
+    );
 }
 
 // BrowserSync
 function browserSync(done) {
     browsersync.init({
         server: {
-            baseDir: "./dist/"
+            baseDir: './dist/'
         }
     });
     done();
@@ -140,55 +150,66 @@ function preTemplateChanges() {
     config = requireUncached('./config.json');
     var slugs = config.slugs;
     return new Promise((resolve, reject) => {
-        resolve(EXT_HTML.reduce((acc, ext) => {
-            // use !(_)*.html to exclude rendering of the files with prefix "_" (underscore)
-            return gulp.src(PRE_MAIN_TEMPLATES(ext))
-                .pipe(plumber())
-                .pipe(data(function (file) {
-                    var splitToken = "/";
-                    var relative_path = file.path.replace(__dirname + "/templates", "");
-                    var count = relative_path.split(splitToken).length - 2;
-                    var relative_assets_path = Array(count).fill("../").join("") + ASSETS_PATH;
-                    // console.log(relative_assets_path);
+        resolve(
+            EXT_HTML.reduce((acc, ext) => {
+                // use !(_)*.html to exclude rendering of the files with prefix "_" (underscore)
+                return (
+                    gulp
+                        .src(PRE_MAIN_TEMPLATES(ext))
+                        .pipe(plumber())
+                        .pipe(
+                            data(function(file) {
+                                var splitToken = '/';
+                                var relative_path = file.path.replace(__dirname + '/templates', '');
+                                var count = relative_path.split(splitToken).length - 2;
+                                var relative_assets_path =
+                                    Array(count)
+                                        .fill('../')
+                                        .join('') + ASSETS_PATH;
+                                // console.log(relative_assets_path);
 
-                    var obj = {
-                            // Custom global variables for pre compiling HTML templates
-                            css_path:   relative_assets_path + "css/",
-                            js_path:    relative_assets_path + "js/",
-                            lib_path:   relative_assets_path + "libs/",
-                            assets:     relative_assets_path,
-                            img_path:   relative_assets_path + "images/",
-                            svgs:       svgs,
-                            fs:         fs,
-                            /* The below setting is used to hide ".html" extension in url paths */
-                            /* It will generate a folder with file's name and insert the content in index.html file */
-                            /* Example: if you pass "home.html", it will compile to "home/index.html" */
-                            // ext: '/index.html'
-                    };
-                    return obj;
-                }))
-                .pipe(htmlCompilers[ext]())
-                .pipe(prettify({ indent_size: 4 }))
-                .pipe(rename(function (obj) {
-                    if (removeHtmlExtension) {
-                        if (obj.basename !== 'index') {
-                            var x = (slugs[obj.basename] || {});
-                            values.title = x.title;
-                            var slug = x.slug;
-                            if (!slug) {
-                                slug = obj.basename;
-                            }
-                            obj.dirname = slug;
-                            obj.basename = "index";
-                        }
-                    }
-                }))
-                // .on('error', swallowError)
-                .pipe(gulp.dest('dist'))
-                .pipe(browsersync.stream());
-
-        }))
-    })
+                                var obj = {
+                                    // Custom global variables for pre compiling HTML templates
+                                    css_path: relative_assets_path + 'css/',
+                                    js_path: relative_assets_path + 'js/',
+                                    lib_path: relative_assets_path + 'libs/',
+                                    assets: relative_assets_path,
+                                    img_path: relative_assets_path + 'images/',
+                                    svgs: svgs,
+                                    fs: fs
+                                    /* The below setting is used to hide ".html" extension in url paths */
+                                    /* It will generate a folder with file's name and insert the content in index.html file */
+                                    /* Example: if you pass "home.html", it will compile to "home/index.html" */
+                                    // ext: '/index.html'
+                                };
+                                return obj;
+                            })
+                        )
+                        .pipe(htmlCompilers[ext]())
+                        .pipe(prettify({ indent_size: 4 }))
+                        .pipe(
+                            rename(function(obj) {
+                                if (removeHtmlExtension) {
+                                    if (obj.basename !== 'index') {
+                                        var x = slugs[obj.basename] || {};
+                                        values.title = x.title;
+                                        var slug = x.slug;
+                                        if (!slug) {
+                                            slug = obj.basename;
+                                        }
+                                        obj.dirname = slug;
+                                        obj.basename = 'index';
+                                    }
+                                }
+                            })
+                        )
+                        // .on('error', swallowError)
+                        .pipe(gulp.dest('dist'))
+                        .pipe(browsersync.stream())
+                );
+            })
+        );
+    });
 }
 
 // Watches changes of sass and templates
@@ -202,7 +223,7 @@ function watchChanges() {
     gulp.watch(
         EXT_HTML.map((ext) => PRE_ALL_TEMPLATES(ext)).concat('config.json'),
         gulp.series('templates', browserSyncReload)
-    )
+    );
     // gulp.watch([PRE_ALL_TEMPLATES(EXT_HTML), 'config.json'],['templates']);
     gulp.watch(SASS_PATH, gulp.series('sass'));
     gulp.watch([SVGS_ALL_PATH, 'config.json'], gulp.series('generate-svg'));
@@ -217,4 +238,4 @@ gulp.task('generate-svg', generateSvg);
 // Dev Tasks
 gulp.task('run', gulp.series('generate-svg', 'sass', 'templates'));
 gulp.task('watch', gulp.series('run', watchChanges));
-gulp.task("default", gulp.series('run', gulp.parallel('watch', browserSync)));
+gulp.task('default', gulp.series('run', gulp.parallel('watch', browserSync)));
